f0ce1476eb1c943e6ff63df6dd2da007a9d9616c
István Zachar <replicatorzed@gmail.com>
2016-09-15 14:51:30 +0200

    Add files via upload

---
+++ Figure3/storkey_retrain.c
@@ -0,0 +1,314 @@
+// By András Szilágyi
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_rng.h>
+
+/******* RANDOM NUMBER GENERATOR **********/
+#define AA 471
+#define B 1586
+#define CC 6988
+#define DD 9689
+#define M 16383
+#define RIMAX 2147483648.0        /* = 2^31 */
+#define RandomInteger (++nd, ra[nd & M] = ra[(nd-AA) & M] ^ ra[(nd-B) & M] ^ ra[(nd-CC) & M] ^ ra[(nd-DD) & M])
+void seed(long seed);
+static long ra[M+1], nd;
+
+
+void seed(long seed)
+{
+ int  i;
+
+ if(seed<0) { puts("SEED error."); exit(1); }
+ ra[0]= (long) fmod(16807.0*(double)seed, 2147483647.0);
+ for(i=1; i<=M; i++)
+ {
+  ra[i] = (long)fmod( 16807.0 * (double) ra[i-1], 2147483647.0);
+ }
+}
+
+long randl(long num)      /* random number between 0 and num-1 */
+{
+ return(RandomInteger % num);
+}
+
+double randd(void)
+{
+ return((double) RandomInteger / RIMAX);
+}
+/****************************************/
+
+
+
+#define N (200)  // number of neurons
+#define NN (20) // number of networks
+#define RETRAINNUM (10)
+#define PN (6) // depth of memory
+#define Th (0.0) // firing threshold
+#define NOUP (100) // number of update steps
+#define mu_l (0.005) // mutation rate of best output (for input) 
+#define mu_T (0.01) // mutation rate of best output (for retraining)
+#define GENERATIONS (3500) // number of generations
+
+float pattern[NN][PN][N];
+float output[NN][N];
+float weight[NN][N][N];
+float optvect[N];
+int optnum;
+unsigned long int zeed = 0;
+
+void init(void)
+{
+  int i,j,k;
+  
+  for(k=0;k<NN;k++)
+    for(i=0;i<N;i++)
+      for(j=0;j<N;j++)
+        weight[k][i][j]=0.0;
+}
+
+void piksr2(int n,float *arr, float *brr)
+{
+  int i,j;
+  float a,b;
+
+  for (j=2;j<=n;j++)
+  {
+    a=arr[j];
+    b=brr[j];
+    i=j-1;
+    while (i > 0 && arr[i] > a)
+    {
+      arr[i+1]=arr[i];
+      brr[i+1]=brr[i];
+      i--;
+    }
+    arr[i+1]=a;
+    brr[i+1]=b;
+  }
+}
+
+
+// set the stored (random) patterns
+void set_stored_pattern(void)
+{
+  int m,n,k;
+
+  for(k=0;k<NN;k++)
+  {
+     for(m = 0; m < PN; m++)
+     {
+       for(n = 0; n < N; n++)
+       {
+         if(randd()>0.5)
+	   pattern[k][m][n]=-1.0;
+	 else
+	   pattern[k][m][n]=1.0;
+
+       }
+    }   
+  }
+
+}
+
+// training network 'm' with vector vec -- palimpsest
+void train_network_pali(int m, float *vec)
+{
+  int i,j;
+  float h[N];
+  
+  for(i=0;i<N;i++)
+  {
+    h[i]=0.0;
+    for(j=0;j<N;j++)    
+      h[i]+=weight[m][i][j]*vec[j];
+  }
+
+  for(i=0;i<N;i++)
+  {
+    for(j=0;j<N;j++)
+    {
+      if(i==j)
+	weight[m][i][j]=0.0;
+      else
+        weight[m][i][j]+=1.0/((float)N)*vec[i]*vec[j]-1.0/((float)N)*vec[i]*h[j]-1.0/((float)N)*vec[j]*h[i];
+    }
+  }
+}
+
+float relat_Hamm(float* in, float* ou)
+{
+  int t;
+  float rhd=0;
+  
+  for(t=0;t<N;t++)
+    if(in[t]!=ou[t])
+      rhd=rhd+1.0;
+    
+  rhd=rhd/(float)N;
+  
+  return(rhd);
+  
+}
+
+// update of neuron 'n' in network 'm'
+void update_output(int m, int n)
+{
+   int t;
+   float h=0.0;
+   
+   for(t = 0; t < N; t++)
+     if(t != n )
+       h += weight[m][n][t] * output[m][t];
+
+   if(h>=Th)
+      output[m][n] =  1.0; // linear threshold
+   else
+      output[m][n] = -1.0;
+
+}
+
+// updating network 'm'
+void update_net(int m) 
+{
+   int t;
+
+   for(t=0;t<N;t++)
+     update_output(m, randl(N));
+}
+
+
+int main(int argc, char** argv)
+{
+ 
+   int k,m,n,up,rou,rr,pro,perm[NN];
+   float vect[N],best[N],pc[NN+1],pcopt,ref[NN+1],vectran[N];
+   gsl_rng *r;
+   
+   
+   seed(12201);
+   zeed = 1051;
+
+   init();
+   r=gsl_rng_alloc(gsl_rng_mt19937);
+   r->type->set(r->state, zeed);
+   for(k=0;k<NN;k++)
+     perm[k]=k;
+
+   set_stored_pattern();
+
+   for(n=0;n<N;n++)
+   {
+     if(randd()<0.5)
+       best[n]=-1.0;
+     else
+       best[n]=1.0;
+   }
+
+
+   for(m=0;m<NN;m++)
+   {
+     for(k=0;k<PN;k++)
+     {     
+       for(n=0;n<N;n++)
+       {
+         vect[n]=pattern[m][k][n];   
+       }
+       train_network_pali(m,vect);
+     }
+   }
+
+
+//start with the same random pattern for all
+
+  for(n=0;n<N;n++)
+  {
+    if(randd()<0.5)
+      vect[n]=-1.0;
+    else
+      vect[n]=1.0;
+  }
+  for(m=0;m<NN;m++)
+    for(n=0;n<N;n++)
+      output[m][n]=vect[n];
+
+
+
+  for(rou=1;rou<=GENERATIONS;rou++)
+  {
+    for(m=0;m<NN;m++)
+      for(up=0;up<NOUP;up++)
+        update_net(m);
+    
+      
+//sort and find the best solution
+
+    optnum=-1;    
+    for(m=0;m<NN;m++)
+    {
+      ref[m+1]=m;
+      for(n=0;n<N;n++)
+	vect[n]=output[m][n];
+      pc[m+1]=relat_Hamm(vect,best);
+    }
+    
+    piksr2(NN,pc,ref);
+   
+    for(m=NN;m>0;m--)
+      if(pc[NN]>pc[m])
+        break;
+      
+    rr=randl(NN-m);
+    pro=(int)ref[NN-rr];
+   
+    pcopt=pc[NN];
+    optnum=pro;
+   
+
+// elitism
+   for(n=0;n<N;n++)
+   {
+     vect[n]=output[optnum][n];
+     optvect[n]=vect[n]; 
+   }
+
+// retraining RETRAINNUM number of networks
+      gsl_ran_shuffle(r,perm,NN,sizeof(int));
+      
+      for(m=0;m<RETRAINNUM;m++)      
+      {
+        for(n=0;n<N;n++)
+        {
+	  vectran[n]=vect[n];
+	  if(randd()<mu_l)
+            vectran[n]=-1.0*vectran[n]; 
+        }
+      
+        rr=perm[m];
+        train_network_pali(rr,vectran);
+      }
+
+// set input for next round
+    for(m=0;m<NN;m++)
+    {
+      for(n=0;n<N;n++)
+      {
+        output[m][n]=vect[n];
+        if(randd()<mu_T)
+          output[m][n]=-1.0*output[m][n];
+      }
+    }
+
+    printf("%d\t%f\n",rou,pcopt);
+    fflush(stdout);
+
+  }
+
+  return(0);
+}
